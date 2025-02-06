f6d65da34b11332b3645722af803c87b773bcc54
Documenter.jl <documenter@juliadocs.github.io>
2025-02-06 22:14:31 +0000

    build based on 5137045

--- dev/gallery/tplot/interactive.jl
+++ dev/gallery/tplot/interactive.jl
@@ -3,21 +3,20 @@ using DimensionalData
 using SpaceTools
 using CairoMakie
 
-t0 = DateTime(2000)
-t1 = DateTime(2000, 1, 10)
+t0 = DateTime(2001, 1, 1)
+t1 = DateTime(2001, 1, 2)
 
 function func(t0, t1)
-    x = t0:Day(1):t1
-    y = sin.(1:length(x))
+    x = t0:Hour(1):t1
+    y = @. sin(2pi * ((x - t0) / Day(1)))
     DimArray(y, Ti(x))
 end
 
 f, axes = tplot(func, t0, t1)
 
 dt = Day(1)
-
 record(f, "assets/interactive.mp4", 1:5; framerate=2) do n
-    tlims!(t0 - n * dt, t1 + n * dt)
+    tlims!(t0, t1 + n * dt)
     sleep(0.5)
 end;
 
